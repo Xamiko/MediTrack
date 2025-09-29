e0cccf6d3727055fa8cfd31c0d5e0ce1c3edb0e9
Xamiko <topoly055@gmail.com>
2025-09-29 11:11:46 +0300

    Add project

---
+++ app/src/main/java/com/medi/meditrack/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.medi.meditrack
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.medi.meditrack.navigation.AppNavHost
+import com.medi.meditrack.screen.GreetingPillScreen
+import com.medi.meditrack.screen.GreetingScreen
+import com.medi.meditrack.ui.theme.MediTrackTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MediTrackTheme(dynamicColor = false) {
+                AppNavHost()
+
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MediTrackTheme {
+        Greeting("Android")
+    }
+}
